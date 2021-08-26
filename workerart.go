087809f996d13a10207c28130baaf60036ee1b5c
HelloShaohua <wu.shaohua@foxmail.com>
2021-08-27 00:51:04 +0800

    feat: feature design (#3)
    * feat: add readme.md file
    
    * feat: feature design (#3)
    
    * feat: add all implements

--- workerart.go
+++ workerart.go
@@ -1,6 +1,22 @@
+// Copyright 2021 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package workerart
 
-import "sync"
+import (
+	"sync"
+)
 
 // WorkerPool worker pool structure.
 type WorkerPool struct {
@@ -13,13 +29,48 @@ type WorkerPool struct {
 	// Workers workersNumber.
 	workersNumber uint
 
+	// Default no processing,
+	// this is，you must implement your own handlers.
+	taskCallback TaskCallback
+
 	// Result of the channel.
 	result chan interface{}
 
 	done chan struct{}
+
+	errors chan error
 }
 
-func (pool *WorkerPool) Done() <-chan struct{} {
+// WorkersProcessing Workers processing the task work.
+func (pool *WorkerPool) WorkersProcessing() {
+
+	// Create multiple workers to complete the work.
+	for i := uint(0); i < pool.workersNumber; i++ {
+		pool.wait.Add(1)
+
+		// Worker task processing.
+		go pool.worker()
+	}
+
+	// Wait for all worker processing to complete.
+	pool.wait.Wait()
+
+	// Close the result channel when all worker processing is complete.
+	close(pool.result)
+}
+
+// Results The result of receiving worker processing.
+func (pool *WorkerPool) Results() <-chan interface{} {
+	return pool.result
+}
+
+// Done Whether the worker pool has completed all work.
+func (pool *WorkerPool) Done() {
+	pool.done <- struct{}{}
+}
+
+// Finished Whether the worker pool has completed all work.
+func (pool *WorkerPool) Finished() <-chan struct{} {
 	return pool.done
 }
 
@@ -29,6 +80,16 @@ func NewWorkerPool(ops ...Option) *WorkerPool {
 		wait:          &sync.WaitGroup{},
 		workersNumber: 100,
 		result:        make(chan interface{}, 100),
+		errors:        make(chan error, 100),
+		done:          make(chan struct{}),
+
+		// Default no processing,
+		// this is，you must implement your own handlers.
+		taskCallback: func(job interface{}) (interface{}, error) {
+			return job, nil
+		},
+
+		jobs: NewJob(),
 	}
 
 	for _, o := range ops {
@@ -36,4 +97,28 @@ func NewWorkerPool(ops ...Option) *WorkerPool {
 	}
 
 	return pool
+}
+
+// worker task processing.
+func (pool *WorkerPool) worker() {
+
+	// Get job from channel.
+	for job := range pool.jobs.GetJobs() {
+
+		// Task processing.
+		task, err := pool.taskCallback(job)
+
+		// If there is an error, an error is sent to the channel.
+		if err != nil {
+			pool.errors <- err
+
+			continue
+		}
+
+		// Send the processing results to the channel.
+		pool.result <- task
+	}
+
+	// Current worker processing completed.
+	pool.wait.Done()
 }
--- workerart_test.go
+++ workerart_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package workerart
 
 import (
@@ -39,22 +53,22 @@ func (suite *WorkerPoolSuite) Test_NewWorkerPool() {
 		pool.jobs.Close()
 	}()
 
-	fmt.Printf("%+v\n", pool)
+	go pool.WorkersProcessing()
 
-	// Gets all job.
 	go func() {
-		ts := 0
-		for val := range pool.jobs.GetJobs() {
+		once := 0
+		for val := range pool.Results() {
 			fmt.Println(val)
-			ts++
+			once++
 		}
-		fmt.Printf("times: %+v\n", ts)
+		fmt.Printf("once: %+v\n", once)
+		pool.Done()
 	}()
 
 	select {
-	case <-pool.Done():
+	case <-pool.Finished():
 		fmt.Printf("done\n")
-	case <-time.After(1 * time.Second):
+	case <-time.After(5 * time.Second):
 		fmt.Printf("timeout\n")
 	}
 }
