01ba5cab5d2732c26bef0e8f632fb3c3bbb64c26
helloshaohua <wu.shaohua@foxmail.com>
2021-08-27 00:48:06 +0800

    feat: add all implements

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ Fast use worker pool process task.
 ```shell script
 go get github.com/fishfinal/workerart
 ```
+
+## What is workerart?
+
+workerart 是一个快速使用协程工作池的实现，而不是当需要有工作池的使用场景时去重复的造轮子，可以提高你的开发效率，同是也不失Go并发处理任务的优雅。workerart 支持：
+
+- 通过选项构建工作池。
+- 通过Jobber接口实现你自己特有的Job.
+- 自定义任务回调函数。
+
+## Why use workerart?
+
+为了优雅的处理多个任务，当然你自己也可以实现工作池，workerart 仅仅只是让你更快速使用工作池来提高任务执行效率。
+
+## How to use?
+
+```go
+
+```
+
--- workerart_test.go
+++ workerart_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package workerart
 
 import (
@@ -39,20 +53,22 @@ func (suite *WorkerPoolSuite) Test_NewWorkerPool() {
 		pool.jobs.Close()
 	}()
 
-	// Gets all job.
+	go pool.WorkersProcessing()
+
 	go func() {
-		ts := 0
-		for val := range pool.jobs.GetJobs() {
+		once := 0
+		for val := range pool.Results() {
 			fmt.Println(val)
-			ts++
+			once++
 		}
-		fmt.Printf("times: %+v\n", ts)
+		fmt.Printf("once: %+v\n", once)
+		pool.Done()
 	}()
 
 	select {
-	case <-pool.Done():
+	case <-pool.Finished():
 		fmt.Printf("done\n")
-	case <-time.After(1 * time.Second):
+	case <-time.After(5 * time.Second):
 		fmt.Printf("timeout\n")
 	}
 }
