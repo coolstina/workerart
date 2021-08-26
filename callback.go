3df9f934b73676430222293e416f832349dc243d
HelloShaohua <wu.shaohua@foxmail.com>
2021-08-27 00:49:35 +0800

    feat: feature design (#3)
    * feat: add all implements

--- callback.go
+++ callback.go
@@ -14,30 +14,5 @@
 
 package workerart
 
-// Job structure.
-type Job struct {
-	list chan interface{}
-}
-
-// NewJob initialize job instance.
-func NewJob() *Job {
-	c := make(chan interface{}, 50)
-	return &Job{list: c}
-}
-
-// AddJob add job into the channel list.
-func (j *Job) AddJob(jobs ...interface{}) {
-	for _, job := range jobs {
-		j.list <- job
-	}
-}
-
-// GetJobs get all jobs from channel list.
-func (j *Job) GetJobs() <-chan interface{} {
-	return j.list
-}
-
-// Close job channel list.
-func (j *Job) Close() {
-	close(j.list)
-}
+// TaskCallback Worker tasks handle callback functions.
+type TaskCallback func(interface{}) (interface{}, error)
